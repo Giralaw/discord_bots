5493636dab8e8c713ad14d2b04c5afedcc85c3a2
Giralaw <al.skeldon@gmail.com>
2024-10-13 17:48:12 -0400

    need to figure out queue and skip issues

--- music_bot/main.py
+++ music_bot/main.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 import os, asyncio
 import logging
+import logging.handlers
 
 from help_cog import help_cog
 from music_cog import music_cog
@@ -16,6 +17,21 @@ bot.remove_command("help")
 
 
 async def main():
+
+    logger = logging.getLogger('discord')
+    logger.setLevel(logging.INFO)
+
+    handler = logging.handlers.RotatingFileHandler(
+        filename='discord.log',
+        encoding='utf-8',
+        maxBytes=32 * 1024 * 1024,  # 32 MiB
+        backupCount=5,  # Rotate through 5 files
+    )
+    dt_fmt = '%Y-%m-%d %H:%M:%S'
+    formatter = logging.Formatter('[{asctime}] [{levelname:<8}] {name}: {message}', dt_fmt, style='{')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
     async with bot:
         await bot.add_cog(help_cog(bot))
         await bot.add_cog(music_cog(bot))
