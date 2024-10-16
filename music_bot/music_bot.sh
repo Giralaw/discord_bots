9ac6b64e1135e0b3c72954505386f5b33aa0ff38
alex <alex@alexchrome.mynetworksettings.com>
2024-10-16 10:34:38 -0400

    bash script added

---
+++ music_bot/music_bot.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+python3 /home/alex/discord_bots/music_bot/main.py >> /home/alex/discord_bots/music_bot/discord.log 2>&1 &
+# python3 /home/alex/discord_bots/music_bot/main.py
+echo "hello world!"
